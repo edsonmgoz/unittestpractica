e5ee09bae77e7302f4ac5176f01346f24ffef9d6
Edson Mollericona Marin <edson@MacBook-Pro-de-Edson.local>
2022-07-11 23:07:02 -0400

    Unit Test with params mocks

--- src/test/java/AuthenticationTest.java
+++ src/test/java/AuthenticationTest.java
@@ -3,7 +3,13 @@ import authentication.CredentialsService;
 import authentication.PermissionService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.util.List;
 
 public class AuthenticationTest {
 
@@ -27,5 +33,39 @@ public class AuthenticationTest {
 
         Mockito.verify(credentialsMock).isValidCredential("admin", "admin");
         Mockito.verify(permissionMock).getPermission("admin");
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "admin,admin",
+            "lucas,lucas",
+            "other,other"
+    })
+    public void verifyAuthWithParams(String user, String pass) {
+        Mockito.when(credentialsMock.isValidCredential(user, pass)).thenReturn(true);
+
+        Mockito.when(permissionMock.getPermission(user)).thenAnswer(new Answer<String>() {
+                public String answer(InvocationOnMock invocation) {
+                    if (user.equals("admin"))
+                        return "CRUD";
+                    else
+                        return "READ-ONLY";
+                }
+            }
+        );
+
+        Authentication authentication = new Authentication();
+        authentication.setCredentials(credentialsMock);
+        authentication.setPermission(permissionMock);
+
+        String exceptedResultAdmin = "user authenticated successfully with permission: [CRUD]";
+        String exceptedResult = "user authenticated successfully with permission: [READ-ONLY]";
+
+        String actualResult = authentication.login(user, pass);
+
+        Assertions.assertTrue(List.of(exceptedResultAdmin, exceptedResult).contains(actualResult));
+
+        Mockito.verify(credentialsMock).isValidCredential(user, pass);
+        Mockito.verify(permissionMock).getPermission(user);
     }
 }
